f8ef717bc429c048ce9e6f7b49fcb63b340778ef
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2022-02-06 05:39:40 -0800

    fasd

--- 04_completion.env.zsh
+++ 04_completion.env.zsh
@@ -93,24 +93,24 @@ _changie()
     local shellCompDirectiveNoFileComp=4
     local shellCompDirectiveFilterFileExt=8
     local shellCompDirectiveFilterDirs=16
-
+    
     local lastParam lastChar flagPrefix requestComp out directive comp lastComp noSpace
     local -a completions
-
+    
     __changie_debug "\n========= starting completion logic =========="
     __changie_debug "CURRENT: ${CURRENT}, words[*]: ${words[*]}"
-
+    
     # The user could have moved the cursor backwards on the command-line.
     # We need to trigger completion from the $CURRENT location, so we need
     # to truncate the command-line ($words) up to the $CURRENT location.
     # (We cannot use $CURSOR as its value does not work when a command is an alias.)
     words=("${=words[1,CURRENT]}")
     __changie_debug "Truncated words[*]: ${words[*]},"
-
+    
     lastParam=${words[-1]}
     lastChar=${lastParam[-1]}
     __changie_debug "lastParam: ${lastParam}, lastChar: ${lastChar}"
-
+    
     # For zsh, when completing a flag with an = (e.g., changie -n=<TAB>)
     # completions must be prefixed with the flag
     setopt local_options BASH_REMATCH
@@ -118,7 +118,7 @@ _changie()
         # We are dealing with a flag with an =
         flagPrefix="-P ${BASH_REMATCH}"
     fi
-
+    
     # Prepare the command to obtain completions
     requestComp="${words[1]} __complete ${words[2,-1]}"
     if [ "${lastChar}" = "" ]; then
@@ -127,20 +127,20 @@ _changie()
         __changie_debug "Adding extra empty parameter"
         requestComp="${requestComp} \"\""
     fi
-
+    
     __changie_debug "About to call: eval ${requestComp}"
-
+    
     # Use eval to handle any environment variables and such
     out=$(eval ${requestComp} 2>/dev/null)
     __changie_debug "completion output: ${out}"
-
+    
     # Extract the directive integer following a : from the last line
     local lastLine
     while IFS='\n' read -r line; do
         lastLine=${line}
     done < <(printf "%s\n" "${out[@]}")
     __changie_debug "last line: ${lastLine}"
-
+    
     if [ "${lastLine[1]}" = : ]; then
         directive=${lastLine[2,-1]}
         # Remove the directive including the : and the newline
@@ -152,16 +152,16 @@ _changie()
         __changie_debug "No directive found.  Setting do default"
         directive=0
     fi
-
+    
     __changie_debug "directive: ${directive}"
     __changie_debug "completions: ${out}"
     __changie_debug "flagPrefix: ${flagPrefix}"
-
+    
     if [ $((directive & shellCompDirectiveError)) -ne 0 ]; then
         __changie_debug "Completion received error. Ignoring completions."
         return
     fi
-
+    
     while IFS='\n' read -r comp; do
         if [ -n "$comp" ]; then
             # If requested, completions are returned with a description.
@@ -169,21 +169,21 @@ _changie()
             # For zsh's _describe, we need to use a : instead of a TAB.
             # We first need to escape any : as part of the completion itself.
             comp=${comp//:/\\:}
-
+            
             local tab=$(printf '\t')
             comp=${comp//$tab/:}
-
+            
             __changie_debug "Adding completion: ${comp}"
             completions+=${comp}
             lastComp=$comp
         fi
     done < <(printf "%s\n" "${out[@]}")
-
+    
     if [ $((directive & shellCompDirectiveNoSpace)) -ne 0 ]; then
         __changie_debug "Activating nospace."
         noSpace="-S ''"
     fi
-
+    
     if [ $((directive & shellCompDirectiveFilterFileExt)) -ne 0 ]; then
         # File extension filtering
         local filteringCmd
@@ -196,10 +196,10 @@ _changie()
             filteringCmd+=" -g $filter"
         done
         filteringCmd+=" ${flagPrefix}"
-
+        
         __changie_debug "File filtering command: $filteringCmd"
         _arguments '*:filename:'"$filteringCmd"
-    elif [ $((directive & shellCompDirectiveFilterDirs)) -ne 0 ]; then
+        elif [ $((directive & shellCompDirectiveFilterDirs)) -ne 0 ]; then
         # File completion for directories only
         local subDir
         subdir="${completions[1]}"
@@ -209,7 +209,7 @@ _changie()
         else
             __changie_debug "Listing directories in ."
         fi
-
+        
         local result
         _arguments '*:dirname:_files -/'" ${flagPrefix}"
         result=$?
@@ -221,7 +221,7 @@ _changie()
         __changie_debug "Calling _describe"
         if eval _describe "completions" completions $flagPrefix $noSpace; then
             __changie_debug "_describe found some completions"
-
+            
             # Return the success of having called _describe
             return 0
         else
@@ -229,7 +229,7 @@ _changie()
             __changie_debug "Checking if we should do file completion."
             if [ $((directive & shellCompDirectiveNoFileComp)) -ne 0 ]; then
                 __changie_debug "deactivating file completion"
-
+                
                 # We must return an error code here to let zsh know that there were no
                 # completions found by _describe; this is what will trigger other
                 # matching algorithms to attempt to find completions.
@@ -238,7 +238,7 @@ _changie()
             else
                 # Perform file completion
                 __changie_debug "Activating file completion"
-
+                
                 # We must return the result of this command, so it must be the
                 # last command, or else we must store its result to return it.
                 _arguments '*:filename:_files'" ${flagPrefix}"
@@ -249,7 +249,16 @@ _changie()
 
 # don't run the completion function when being source-ed or eval-ed
 if [ "$funcstack[1]" = "_changie" ]; then
-	_changie
+    _changie
 fi
 
 source /usr/share/fzf/completion.zsh
+# --------------------------------------------------- #
+# --------------------------------------------------- #
+# --------------------------------------------------- #
+fasd_cache="$HOME/.fasd-init"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+    fasd --init auto >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
