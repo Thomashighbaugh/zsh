f38650819569f4566aef29bbcbd04f41a0b54bab
Thomashighbaugh <thighbaugh@zoho.com>
2022-06-26 19:59:11 -0700

    options in loop

--- 01_options.env.zsh
+++ 01_options.env.zsh
@@ -1,82 +1,92 @@
 #!/bin/env zsh
+################################################################################
+## Modules #####################################################################
+################################################################################
+zmodload zsh/zle
+zmodload zsh/zpty
+zmodload zsh/complist
+
+autoload _vi_search_fix
+
+autoload -Uz compinit
+compinit
+
+autoload -Uz colors
+colors
+
+zle -N _vi_search_fix
+zle -N _sudo_command_line
 
 ################################################################################
 ## Options #####################################################################
 ################################################################################
-# make cd push the old directory onto the directory stack.
-setopt auto_pushd
-# avoid "beep"ing
-setopt nobeep
-# if only directory path is entered, cd there.
-setopt autocd
-setopt autoremoveslash
-setopt cdablevars
-# Make cd = pushd
-setopt AUTO_PUSHD
-setopt PUSHD_IGNORE_DUPS
-setopt PUSHD_MINUS
-# Completion Options
-setopt menucomplete
-setopt globdots
-setopt EXTENDED_GLOB
-setopt GLOB_COMPLETE
-setopt NO_CASE_GLOB
-setopt NUMERIC_GLOB_SORT
 
-unsetopt autoparamslash
-# Array expansion with parameters
-setopt rcexpandparam
-# automatically remove duplicates from these arrays
-typeset -U path cdpath fpath manpath
-
-# not just at the end
-setopt completeinword
-
-# use zsh style word splitting
-setopt noshwordsplit
-# allow use of comments in interactive code
-setopt interactivecomments
-
-setopt notify
-setopt allexport
-# Automatically use menu completion
-setopt auto_menu
-setopt menucomplete
-# Move cursor to end if word had one match
-setopt always_to_end
-setopt pushdtohome
-setopt recexact
-setopt pushdsilent
-setopt pushdminus
-
-# whenever a command completion is attempted, make sure the entire command path
-# is hashed first.
-setopt hash_list_all
-
-# no c-s/c-q output freezing
-setopt noflowcontrol
-
-# Don't warn about running processes when exiting
-setopt nocheckjobs
-setopt longlistjobs
-
-# auto correct mistakes
-setopt correct
-# enable filename expansion for arguments of the form ‘anything=expression’
-setopt magicequalsubst
-# hide error message if there is no match for the pattern
-setopt nonomatch
-
-# enable command substitution in prompt
-setopt promptsubst
-# Include job PID
-setopt longlistjobs
-# Automatically list choices on ambiguous completion
-setopt auto_list
-
-unsetopt bgnice
+while read -r opt; do
+  setopt $opt
+done <<-EOF
+ALLEXPORT
+ALWAYS_TO_END
+APPEND_HISTORY
+AUTO_LIST
+AUTO_MENU
+AUTO_PARAM_SLASH
+AUTO_PUSHD
+AUTOCD
+AUTOREMOVESLASH
+CDABLEVARS
+COMPLETE_IN_WORD
+COMPLETEINWORD
+CORRECT
+EXTENDED_GLOB
+EXTENDED_HISTORY
+GLOB_COMPLETE
+GLOBDOTS
+HASH_LIST_ALL
+HIST_EXPIRE_DUPS_FIRST
+HIST_IGNORE_ALL_DUPS
+HIST_IGNORE_SPACE
+HIST_NO_FUNCTIONS
+HIST_REDUCE_BLANKS
+HIST_SAVE_NO_DUPS
+INC_APPEND_HISTORY
+INTERACTIVE_COMMENTS
+INTERACTIVECOMMENTS
+LONGLISTJOBS
+MAGICEQUALSUBST
+MAILWARN
+MENUCOMPLETE
+NO_CASE_GLOB
+NO_MENU_COMPLETE
+NOBEEP
+NOCHECKJOBS
+NOFLOWCONTROL
+NOHUP
+NONOMATCH
+NOSHWORDSPLIT
+NOTIFY
+NUMERIC_GLOB_SORT
+PROMPTSUBST
+PUSHD_IGNORE_DUPS
+PUSHD_MINUS
+PUSHDMINUS
+PUSHDSILENT
+PUSHDTOHOME
+RCEXPANDPARAM
+RECEXACT
+RM_STAR_WAIT
+SHARE_HISTORY
+EOF
 
 WORDCHARS=${WORDCHARS//\//} # Don't consider certain characters part of the word
-###########################################################################
-####################################################################
-###########################################################################
+typeset -U path cdpath fpath manpath
+
+while read -r opt; do
+  unsetopt $opt
+done <<-EOF
+AUTOPARAMSLASH
+BGNICE
+CORRECT
+EQUALS
+FLOWCONTROL
+NOMATCH
+EOF
